b60f7c1e71df79b2e98406466dc88099934e1373
BDKSoftware <kookcodes@gmail.com>
2024-02-13 10:54:20 -0700

    Changes

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { CameraView, useCameraPermissions } from "expo-camera/next";
+import { useState, useEffect } from "react";
+import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
+  const [facing, setFacing] = useState("back");
+  const [permission, requestPermission] = useCameraPermissions();
+
+  useEffect(() => {
+    if (permission === "undetermined") {
+      requestPermission();
+    }
+  }, [permission, requestPermission]);
+
+  function toggleCameraFacing() {
+    setFacing((current) => (current === "back" ? "front" : "back"));
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CameraView style={styles.camera} facing={facing}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
+            <Text style={styles.text}>Flip Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </CameraView>
     </View>
   );
 }
@@ -13,8 +32,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+    marginBottom: 36,
+  },
+  button: {
+    alignSelf: "center",
+    padding: 20,
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  text: {
+    fontSize: 20,
+    color: "#fff",
   },
 });
